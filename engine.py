6d09f13b229802de4d07e21aeba9d14196da2817
Big Bear <48781111+linbearababy@users.noreply.github.com>
2023-03-22 16:20:18 -0400

    Update engine.py

--- engine.py
+++ engine.py
@@ -114,3 +114,52 @@ def evaluate(data_loader, model, device):
           .format(top1=metric_logger.acc1, top5=metric_logger.acc5, losses=metric_logger.loss))
 
     return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
+  
+  
+ ##modify the aug method on the one image
+@torch.no_grad()
+def evaluate(data_loader, model, device):
+    criterion = torch.nn.CrossEntropyLoss()
+
+    ## Define augmentation functions (add)
+    tta_transforms = [
+        torchvision.transforms.RandomResizedCrop(224),
+        torchvision.transforms.RandomHorizontalFlip(),
+        torchvision.transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
+        torchvision.transforms.RandomGrayscale(p=0.2),
+        torchvision.transforms.RandomRotation(15),
+        torchvision.transforms.RandomPerspective(),
+        torchvision.transforms.RandomAffine(15)
+    ]
+    
+    ## Create a composite transform that applies all the augmentation functions (add)
+    composite_transform = torchvision.transforms.Compose(tta_transforms)
+
+    # switch to evaluation mode
+    model.eval()
+
+    metric_logger = utils.MetricLogger(delimiter="  ")
+    header = 'Test:'
+
+    for images, target in metric_logger.log_every(data_loader, 10, header):
+        images = images.to(device, non_blocking=True)
+        target = target.to(device, non_blocking=True)
+
+        ## apply test-time augmentation with the composite transform (update)
+        with torch.cuda.amp.autocast():
+            output = model(composite_transform(images))
+
+        loss = criterion(output, target)
+        acc1, acc5 = accuracy(output, target, topk=(1, 5))
+
+        batch_size = images.shape[0]
+        metric_logger.update(loss=loss.item())
+        metric_logger.meters['acc1'].update(acc1.item(), n=batch_size)
+        metric_logger.meters['acc5'].update(acc5.item(), n=batch_size)
+
+    # gather the stats from all processes
+    metric_logger.synchronize_between_processes()
+    print('* Acc@1 {top1.global_avg:.3f} Acc@5 {top5.global_avg:.3f} loss {losses.global_avg:.3f}'
+          .format(top1=metric_logger.acc1, top5=metric_logger.acc5, losses=metric_logger.loss))
+
+    return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
